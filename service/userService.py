89b1f1920d5e539d5e495e63a99cd303dbc1b3c6
homelala <homelala@naver.com>
2022-05-10 16:45:08 +0900

    add: env setting

--- service/userService.py
+++ service/userService.py
@@ -1,6 +1,9 @@
 from dto.UserCreateDto import UserCreateDto
 from domain.User import User
+from repository import userRepository
 
 
 def userSignUp(form: UserCreateDto):
     user = User(UserCreateDto.get__name(), UserCreateDto.get__email, UserCreateDto.get__passwd)
+    userRepository.save(user)
+    return user

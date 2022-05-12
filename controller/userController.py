d48fcee9224cd88513b32b24e920145f30c14601
homelala <homelala@naver.com>
2022-05-12 13:50:52 +0900

    add: bluePrint -> classFul로 변경

--- controller/userController.py
+++ controller/userController.py
@@ -1,7 +1,7 @@
 from bson import json_util
 from flask import request, jsonify, Blueprint, make_response
 import json
-
+from flask_classful import route, FlaskView
 from dto.ResponseDto import ResponseDto
 from dto.UserCreateDto import UserCreateDto
 from dto.UserLogInDto import UserLogInDto
@@ -10,34 +10,33 @@ from service import userService
 from utils.CustomException import CustomException
 from utils.ErrorResponseDto import ErrorResponseDto
 
-userApp = Blueprint("userApp", __name__)
 
+class UserController(FlaskView):
+    route_base = "/user"
 
-@userApp.route("/signUp", methods=["POST"])
-def userSignUP():
-    try:
-        body = request.get_json()
-        userId = userService.userSignUp(UserCreateDto(body["name"], body["email"], body["passwd"]))
-        return ResponseDto(200, "회원 가입 완료", {"userId": json.loads(json_util.dumps(userId))["$oid"]}).toJSON(), 200
-    except CustomException as e:
-        return ErrorResponseDto(e.message, e.statusCode).toJSON()
+    @route("/signUp")
+    def userSignUP(self):
+        try:
+            body = request.get_json()
+            userId = userService.userSignUp(UserCreateDto(body["name"], body["email"], body["passwd"]))
+            return ResponseDto(200, "회원 가입 완료", {"userId": json.loads(json_util.dumps(userId))["$oid"]}).toJSON(), 200
+        except CustomException as e:
+            return ErrorResponseDto(e.message, e.statusCode).toJSON()
 
+    @route("/logIn", methods=["POST"])
+    def userLogIn(self):
+        try:
+            body = request.get_json()
+            userId = userService.userLogIn(UserLogInDto(body["email"], body["passwd"]))
+            return ResponseDto(200, "로그인 완료", {"userId": json.loads(json_util.dumps(userId))["$oid"]}).toJSON(), 200
+        except CustomException as e:
+            return ErrorResponseDto(e.message, e.statusCode).toJSON()
 
-@userApp.route("/logIn", methods=["POST"])
-def userLogIn():
-    try:
-        body = request.get_json()
-        userId = userService.userLogIn(UserLogInDto(body["email"], body["passwd"]))
-        return ResponseDto(200, "로그인 완료", {"userId": json.loads(json_util.dumps(userId))["$oid"]}).toJSON(), 200
-    except CustomException as e:
-        return ErrorResponseDto(e.message, e.statusCode).toJSON()
-
-
-@userApp.route("/update", methods=["POST"])
-def userUpdateInfo():
-    try:
-        body = request.get_json()
-        userService.userUpdateInfo(UserUpdateInfoDto(body["token"], body["name"]))
-        return ResponseDto(200, "회원 정보 수정이 완료되었습니다.").toJSON(), 200
-    except CustomException as e:
-        return ErrorResponseDto(e.message, e.statusCode).toJSON()
+    @route("/update", methods=["POST"])
+    def userUpdateInfo(self):
+        try:
+            body = request.get_json()
+            userService.userUpdateInfo(UserUpdateInfoDto(body["token"], body["name"]))
+            return ResponseDto(200, "회원 정보 수정이 완료되었습니다.").toJSON(), 200
+        except CustomException as e:
+            return ErrorResponseDto(e.message, e.statusCode).toJSON()
--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 from flask import Flask
+from flask_classful import FlaskView, route
 from flask_restx import Api, Resource
-from controller import userController
+from controller.userController import UserController
 
 app = Flask(__name__)
-app.register_blueprint(userController.userApp, url_prefix="/user")
+# app.register_blueprint(userController.userApp, url_prefix="/user")
 api = Api(app, version="1.0", title="게시판 Api", description="API description")
 
 
-@app.route("/test")
-def home():
-    return "welcome"
+class Main(FlaskView):
+    @route("/test")
+    def home(self):
+        return "welcome"
 
+
+Main.register(app)
+UserController.register(app)
 
 if __name__ == "__main__":
     app.run("127.0.0.1", port=8080, debug=True)
